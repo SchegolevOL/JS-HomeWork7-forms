34b3ed61fe3eafa2c4b8b7a9400e4d843e1b26aa
Oleg Schegol <olschegolev@yahoo.com>
2023-02-27 17:24:43 +0500

    add coock

--- js/scripts.js
+++ js/scripts.js
@@ -73,3 +73,41 @@ document.querySelector(".tasks").addEventListener("click", (event) => {
   }
   localStorage.setItem("tasks", JSON.stringify(tasks));
 });
+
+let body = document.querySelector('body');
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    body.style.setProperty('background-color', document.cookie.split(';')[0].split('=')[1]);
+});
+
+document.querySelector('#redBtn').addEventListener('click', () => {
+    console.log('red');
+
+    document.cookie = "theme=red;max-age=86400";
+    body.style.setProperty('background-color', 'red');
+    document.querySelector('.form-control').style.setProperty('background-color', 'yellow');
+   
+
+});
+
+document.querySelector('#blueBtn').addEventListener('click', () => {
+    console.log('blue');
+    document.cookie = "theme=blue;max-age=86400";
+    body.style.setProperty('background-color', 'blue');
+    document.querySelector('.form-control').style.setProperty('background-color', 'green');
+});
+
+document.querySelector('#greenBtn').addEventListener('click', () => {
+    console.log('green');
+    document.cookie = "theme=green;max-age=86400";
+    body.style.setProperty('background-color', 'green');
+    document.querySelector('.form-control').style.setProperty('background-color', 'blue');
+});
+
+document.querySelector('#yellowBtn').addEventListener('click', () => {
+    console.log('yellow');
+    document.cookie = "theme=yellow;max-age=86400";
+    body.style.setProperty('background-color', 'yellow');
+    document.querySelector('.form-control').style.setProperty('background-color', 'red');
+});
